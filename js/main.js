17b6a9d07fa71bbddd8cd3f5a36f3d610668cbb2
Maël Brunet <maelbrunet@gmail.com>
2017-09-18 01:39:02 +0200

    Add missing trailtext

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ const tabs = document.querySelectorAll('.js-tab');
 // This function updates a DOM element with data fetched from the Content API. It should be passed three arguments :
 // * the listTopic as a string
 // * the DOM element that should be updated with the new data
-// * (Optional) the maximum number of items to be displayed, as a number
+// * (Optional) the maximum number of items to be displayed, as an integer
 const fillListFromAPI = (listTopic, element, maxItems) =>
 {
   const numItems = maxItems <= 10 ? maxItems : 10;
   let updatedNewsList = '';
 
-  fetch(`http://content.guardianapis.com/search?section=${listTopic}&api-key=9wur7sdh84azzazdt3ye54k4`)
+  fetch(`http://content.guardianapis.com/search?show-fields=all&section=${listTopic}&api-key=9wur7sdh84azzazdt3ye54k4`)
     .then(function (response) {
       return response.json()
     }).then(function (json) {
@@ -20,7 +20,11 @@ const fillListFromAPI = (listTopic, element, maxItems) =>
     const results = json.response.results;
 
     for (let i = 0; i < numItems; i++) {
-      updatedNewsList += `<li><a href="${results[i].webUrl}">${results[i].webTitle}</a></li>`;
+      updatedNewsList +=
+        `<li>
+            <a href="${results[i].webUrl}">${results[i].webTitle}</a>
+            <div>${results[i].fields.trailText}</div>
+        </li>`;
     }
   }).catch(function (ex) {
     // @TODO handle fetch error
