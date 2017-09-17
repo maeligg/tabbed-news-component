71cb9c04e60ea2cd0ad38011f5d42cd979104544
Maël Brunet <maelbrunet@gmail.com>
2017-09-18 00:34:22 +0200

    Handle the tabs behaviour + reload the data on click

--- js/main.js
+++ js/main.js
@@ -3,29 +3,50 @@ require('fetch-ie8');
 
 const tabs = document.querySelectorAll('.js-tab');
 
-// For each tab, fetch the corresponding news items
-[].forEach.call(tabs, function(tab) {
-  const topic = tab.getAttribute('data-topic');
-  const newsList = document.querySelector('.js-news-items').querySelector(`[data-topic="${topic}"]`);
+// This function updates a DOM element with data fetched from the Content API. It must be passed two arguments :
+// * the listTopic as a string
+// * the DOM element that should be updated with the new data
+const fillListFromAPI = (listTopic, element) =>
+{
   let updatedNewsList = '';
 
-  fetch(`http://content.guardianapis.com/search?section=${topic}&api-key=9wur7sdh84azzazdt3ye54k4`)
-    .then(function(response) {
+  fetch(`http://content.guardianapis.com/search?section=${listTopic}&api-key=9wur7sdh84azzazdt3ye54k4`)
+    .then(function (response) {
       return response.json()
-    }).then(function(json) {
-      // @TODO handle the case in which we receive less than 5 items from the API
-      const results = json.response.results;
-      for (var key in results) {
-        if(results.hasOwnProperty(key)) {
-          updatedNewsList += `<li><a href="${results[key].webUrl}">${results[key].webTitle}</a></li>`;
-        }
+    }).then(function (json) {
+    // @TODO handle the case in which we receive less than 5 items from the API
+    const results = json.response.results;
+    for (var key in results) {
+      if (results.hasOwnProperty(key)) {
+        updatedNewsList += `<li><a href="${results[key].webUrl}">${results[key].webTitle}</a></li>`;
       }
-  }).catch(function(ex) {
-    // @TODO handle fetch error
-  }).then(function() {
-    // Check if the new string actually contains something, otherwise leave the DOM unchanged
-    if(updatedNewsList) {
-      newsList.innerHTML = updatedNewsList;
     }
+  }).catch(function (ex) {
+    // @TODO handle fetch error
+  }).then(function () {
+    // Check if the new string actually contains something, otherwise leave the DOM unchanged
+    if (updatedNewsList) {
+      element.innerHTML = updatedNewsList;
+    }
+  });
+}
+
+
+[].forEach.call(tabs, function(tab) {
+  const topic = tab.getAttribute('data-topic');
+  const allNewsLists = document.querySelectorAll('.js-news-list');
+  const newsList = document.querySelector('.js-news-items').querySelector(`[data-topic="${topic}"]`);
+
+  // First we fetch data for all tabs on page load
+  fillListFromAPI(topic, newsList);
+
+  // Then we bind the event listener to handle the tabs being clicked
+  tab.addEventListener('click', function() {
+    [].forEach.call(allNewsLists, function(currentNewsList) {
+      currentNewsList.classList.remove('active');
+    });
+
+    newsList.classList.add('active');
+    fillListFromAPI(topic, newsList);
   });
 });
