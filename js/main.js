55e1284e0aa857c0aff01d86aeb792c91152a274
Maël Brunet <maelbrunet@gmail.com>
2017-09-18 01:01:25 +0200

    Only fetch 5 items + add some doc

--- js/main.js
+++ js/main.js
@@ -3,23 +3,24 @@ require('fetch-ie8');
 
 const tabs = document.querySelectorAll('.js-tab');
 
-// This function updates a DOM element with data fetched from the Content API. It must be passed two arguments :
+// This function updates a DOM element with data fetched from the Content API. It should be passed three arguments :
 // * the listTopic as a string
 // * the DOM element that should be updated with the new data
-const fillListFromAPI = (listTopic, element) =>
+// * (Optional) the maximum number of items to be displayed, as a number
+const fillListFromAPI = (listTopic, element, maxItems) =>
 {
+  const numItems = maxItems <= 10 ? maxItems : 10;
   let updatedNewsList = '';
 
   fetch(`http://content.guardianapis.com/search?section=${listTopic}&api-key=9wur7sdh84azzazdt3ye54k4`)
     .then(function (response) {
       return response.json()
     }).then(function (json) {
-    // @TODO handle the case in which we receive less than 5 items from the API
+    // @TODO handle the case in which we receive less than 10 items from the API
     const results = json.response.results;
-    for (var key in results) {
-      if (results.hasOwnProperty(key)) {
-        updatedNewsList += `<li><a href="${results[key].webUrl}">${results[key].webTitle}</a></li>`;
-      }
+
+    for (let i = 0; i < numItems; i++) {
+      updatedNewsList += `<li><a href="${results[i].webUrl}">${results[i].webTitle}</a></li>`;
     }
   }).catch(function (ex) {
     // @TODO handle fetch error
@@ -36,9 +37,10 @@ const fillListFromAPI = (listTopic, element) =>
   const topic = tab.getAttribute('data-topic');
   const allNewsLists = document.querySelectorAll('.js-news-list');
   const newsList = document.querySelector('.js-news-items').querySelector(`[data-topic="${topic}"]`);
+  const numItemsInList = 5;
 
   // First we fetch data for all tabs on page load
-  fillListFromAPI(topic, newsList);
+  fillListFromAPI(topic, newsList, numItemsInList);
 
   // Then we bind the event listener to handle the tabs being clicked
   tab.addEventListener('click', function() {
@@ -47,6 +49,6 @@ const fillListFromAPI = (listTopic, element) =>
     });
 
     newsList.classList.add('active');
-    fillListFromAPI(topic, newsList);
+    fillListFromAPI(topic, newsList, numItemsInList);
   });
 });
